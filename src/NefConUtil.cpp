fe0979b4edb4d246a4226b059a06b1d58196c034
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-08 01:28:49 +0200

    Removed unused code

--- src/NefConUtil.cpp
+++ src/NefConUtil.cpp
@@ -901,18 +901,6 @@ static bool GUIDFromString(const std::string& input, GUID* guid)
     return true;
 }
 
-namespace Color
-{
-    const std::string RESET = "\033[0m";
-    const std::string RED = "\033[31m";
-    const std::string GREEN = "\033[32m";
-    const std::string YELLOW = "\033[33m";
-    const std::string BLUE = "\033[34m";
-    const std::string MAGENTA = "\033[35m";
-    const std::string CYAN = "\033[36m";
-    const std::string WHITE = "\033[37m";
-}
-
 static void CustomizeEasyLoggingColeredConsole()
 {
     el::Configurations conf;
