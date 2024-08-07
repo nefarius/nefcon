a8bb7e8ce8592eb8484ee3ef3a9295f6105092f6
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-08 01:29:48 +0200

    Update ColorLogging.hpp

--- src/ColorLogging.hpp
+++ src/ColorLogging.hpp
@@ -5,17 +5,16 @@
 #include "colorwin.hpp"
 #include <easylogging++.h>
 
-using namespace colorwin;
 
 class ConsoleColorLogDispatchCallback : public el::LogDispatchCallback
 {
-    std::unordered_map<el::Level, CW_COLORS> logLevelToColor = {
-        {el::Level::Debug, white},
-        {el::Level::Info, green},
-        {el::Level::Warning, yellow},
-        {el::Level::Error, red},
-        {el::Level::Fatal, magenta},
-        {el::Level::Verbose, white}
+    std::unordered_map<el::Level, colorwin::CW_COLORS> logLevelToColor = {
+        {el::Level::Debug, colorwin::white},
+        {el::Level::Info, colorwin::green},
+        {el::Level::Warning, colorwin::yellow},
+        {el::Level::Error, colorwin::red},
+        {el::Level::Fatal, colorwin::magenta},
+        {el::Level::Verbose, colorwin::white}
     };
 
 protected:
@@ -40,7 +39,7 @@ protected:
 
         // scoped color
         {
-            std::cout << timestamp << " " << color(logLevelToColor[logMessage->level()]) << level;
+            std::cout << timestamp << " " << colorwin::color(logLevelToColor[logMessage->level()]) << level;
         }
         std::cout << " " << message << '\n';
     }
