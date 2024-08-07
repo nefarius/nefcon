1d693e72fd59e335ce931ec4eb40e3a6dbc8087f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-08-07 16:24:26 +0200

    Added HDEVINFOHandleGuard

--- src/ScopeGuards.hpp
+++ src/ScopeGuards.hpp
@@ -73,4 +73,73 @@ namespace nefarius::util
     private:
         HINF handle_;
     };
+
+    class HDEVINFOHandleGuard
+    {
+    public:
+        // Constructor takes the HINF handle to manage
+        explicit HDEVINFOHandleGuard(HDEVINFO handle) : handle_(handle)
+        {
+        }
+
+        // Destructor releases the HINF resource
+        ~HDEVINFOHandleGuard()
+        {
+            if (handle_ != INVALID_HANDLE_VALUE)
+            {
+                SetupDiDestroyDeviceInfoList(handle_);
+            }
+        }
+
+        // Disable copy constructor and copy assignment operator
+        HDEVINFOHandleGuard(const HDEVINFOHandleGuard&) = delete;
+        HDEVINFOHandleGuard& operator=(const HDEVINFOHandleGuard&) = delete;
+
+        // Move constructor
+        HDEVINFOHandleGuard(HDEVINFOHandleGuard&& other) noexcept : handle_(other.handle_)
+        {
+            other.handle_ = INVALID_HANDLE_VALUE;
+        }
+
+        // Move assignment operator
+        HDEVINFOHandleGuard& operator=(HDEVINFOHandleGuard&& other) noexcept
+        {
+            if (this != &other)
+            {
+                // Release current resource
+                if (handle_ != INVALID_HANDLE_VALUE)
+                {
+                    SetupDiDestroyDeviceInfoList(handle_);
+                }
+                // Transfer ownership
+                handle_ = other.handle_;
+                other.handle_ = INVALID_HANDLE_VALUE;
+            }
+            return *this;
+        }
+
+        // Function to manually release the handle, if needed
+        void release()
+        {
+            if (handle_ != INVALID_HANDLE_VALUE)
+            {
+                SetupDiDestroyDeviceInfoList(handle_);
+                handle_ = INVALID_HANDLE_VALUE;
+            }
+        }
+
+        // Accessor for the handle
+        HDEVINFO get() const
+        {
+            return handle_;
+        }
+
+        bool is_invalid() const
+        {
+            return handle_ == INVALID_HANDLE_VALUE;
+        }
+
+    private:
+        HDEVINFO handle_;
+    };
 }
