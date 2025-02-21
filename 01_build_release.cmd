f5dd078b3d3045be3127d962c62b1c4b2710bb9a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-21 22:40:39 +0100

    Build scripts updates

--- 01_build_release.cmd
+++ 01_build_release.cmd
@@ -7,16 +7,16 @@ pushd "%MYDIR%"
 where msbuild > nul 2>&1 && (
     echo Starting build
 
-    msbuild /p:Configuration="Release (WinMain)" /p:Platform=x64 .\NefConUtil.vcxproj
-	msbuild /p:Configuration="Release (WinMain)" /p:Platform=ARM64 .\NefConUtil.vcxproj
-    msbuild /p:Configuration="Release (WinMain)" /p:Platform=Win32 .\NefConUtil.vcxproj
+    msbuild /p:Configuration="Release (WinMain)" /p:Platform=x64 .\NefConUtil.vcxproj -verbosity:minimal
+	msbuild /p:Configuration="Release (WinMain)" /p:Platform=ARM64 .\NefConUtil.vcxproj -verbosity:minimal
+    msbuild /p:Configuration="Release (WinMain)" /p:Platform=Win32 .\NefConUtil.vcxproj -verbosity:minimal
 
-    msbuild /p:Configuration="Release (Console)" /p:Platform=x64 .\NefConUtil.vcxproj
-	msbuild /p:Configuration="Release (Console)" /p:Platform=ARM64 .\NefConUtil.vcxproj
-    msbuild /p:Configuration="Release (Console)" /p:Platform=Win32 .\NefConUtil.vcxproj
+    msbuild /p:Configuration="Release (Console)" /p:Platform=x64 .\NefConUtil.vcxproj -verbosity:minimal
+	msbuild /p:Configuration="Release (Console)" /p:Platform=ARM64 .\NefConUtil.vcxproj -verbosity:minimal
+    msbuild /p:Configuration="Release (Console)" /p:Platform=Win32 .\NefConUtil.vcxproj -verbosity:minimal
 
 ) || (
-    echo MSBuild not found, please make sure you called the "Setup-VS2022" snippet before!
+    echo MSBuild not found, please make sure you called the "Setup-VS(2022)" snippet before!
 )
 
 popd
--- 02_sign.cmd
+++ 02_sign.cmd
@@ -4,7 +4,12 @@
 set MYDIR=%~dp0
 pushd "%MYDIR%"
 
-wdkwhere run signtool sign /v /n "Nefarius Software Solutions e.U." /tr http://timestamp.digicert.com /fd sha256 /td sha256 ".\bin\x64\*.exe" ".\bin\ARM64\*.exe" ".\bin\x86\*.exe"
+wdkwhere run signtool sign /v /n "Nefarius Software Solutions e.U." /a ^
+    /tr http://timestamp.digicert.com ^
+    /fd sha256 /td sha256 ^
+    ".\bin\x64\*.exe" ^
+    ".\bin\ARM64\*.exe" ^
+    ".\bin\x86\*.exe"
 
 popd
 endlocal
